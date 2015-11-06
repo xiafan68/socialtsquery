3ffe7b92bac22a0bdd921dff53d72ca9a06ec3b6
xiafan <xiafan68@gmail.com>
2015-11-06 13:04:28 +0800

    增加代码测试iter的正确性

--- src/test/java/core/lsmo/octree/OctreePostingListIterTest.java
+++ src/test/java/core/lsmo/octree/OctreePostingListIterTest.java
@@ -1,127 +1,115 @@
 package core.lsmo.octree;
 
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.Iterator;
 
 import org.apache.log4j.PropertyConfigurator;
 import org.junit.Assert;
 import org.junit.Test;
 
-import segmentation.Interval;
 import Util.Configuration;
 import common.MidSegment;
 import core.commom.Encoding;
-import core.commom.Point;
-import core.lsmo.DiskSSTableReader;
+import core.lsmo.DiskSSTableBDBReader;
 import core.lsmo.OctreeBasedLSMTFactory;
 import core.lsmt.IMemTable.SSTableMeta;
 import core.lsmt.LSMTInvertedIndex;
 import core.lsmt.WritableComparableKey;
+import segmentation.Interval;
 
 public class OctreePostingListIterTest {
+
+	public int[] ExpectedResult(WritableComparableKey key, DiskSSTableBDBReader reader, Interval window)
+			throws IOException {
+		int[] ret = new int[] { 0, 0 };
+		System.out.println(key);
+		Encoding code = null;
+		OctreeNode cur = null;
+		// FileOutputStream fos = new FileOutputStream("/tmp/visit.log");
+		// System.setOut(new PrintStream(fos));
+		// 遍历所有的node，找到相关的segs
+		IOctreeIterator scanner = reader.getPostingListScanner(key);
+		while (scanner.hasNext()) {
+			cur = scanner.nextNode();
+			code = cur.getEncoding();
+			if (code.getX() <= window.getEnd() && code.getY() + code.getEdgeLen() >= window.getStart()) {
+				ret[0]++;
+				// System.out.print("hit ");
+				// System.out.println(code);
+				// System.out.println(((DiskOctreeIterator)
+				// scanner).nextBucketID);
+			} else {
+				// System.out.print("not hit ");
+			}
+
+			for (MidSegment seg : cur.getSegs()) {
+				if (seg.getStart() <= window.getEnd() && seg.getEndTime() >= window.getStart()) {
+					ret[1]++;
+				}
+			}
+		}
+		return ret;
+	}
+
+	public int[] queryResult(WritableComparableKey key, DiskSSTableBDBReader reader, Interval window)
+			throws IOException {
+		int[] ret = new int[] { 0, 0 };
+		IOctreeIterator scanner = reader.getPostingListIter(key, window.getStart(), window.getEnd());
+		Encoding code = null;
+		OctreeNode cur = null;
+		while (scanner.hasNext()) {
+			cur = scanner.nextNode();
+			code = cur.getEncoding();
+			if (code.getX() <= window.getEnd() && code.getY() + code.getEdgeLen() >= window.getStart()) {
+				ret[0]++;
+			}
+			for (MidSegment seg : cur.getSegs()) {
+				if (seg.getStart() <= window.getEnd() && seg.getEndTime() >= window.getStart()) {
+					ret[1]++;
+				}
+			}
+		}
+		return ret;
+	}
+
 	@Test
 	public void test() throws IOException {
 		PropertyConfigurator.configure("conf/log4j-server.properties");
 
 		Configuration conf = new Configuration();
-		conf.load("conf/index.conf");
+		conf.load("conf/index_twitter.conf");
 
 		LSMTInvertedIndex index = new LSMTInvertedIndex(conf, OctreeBasedLSMTFactory.INSTANCE);
-		DiskSSTableReader reader = new DiskSSTableReader(index, new SSTableMeta(255, 8));
+		index.init();
+
+		DiskSSTableBDBReader reader = (DiskSSTableBDBReader) index.getSSTableReader(index.getVersion(),
+				new SSTableMeta(127, 7));
 
 		reader.init();
 		Iterator<WritableComparableKey> iter = reader.keySetIter();
 		while (iter.hasNext()) {
 			System.out.println(iter.next());
 		}
-		int ts = 696602;
+		int ts = 676602;
 		int te = 696622;
 		Interval window = new Interval(0, ts, te, 0);
 
-		int expectNodeNum = 0;
+		System.out.println("begin to verify");
+		System.out.flush();
 		iter = reader.keySetIter();
-		HashSet<Encoding> expectCodes = new HashSet<Encoding>();
-		Encoding code = null;
-		Encoding exCode = new Encoding(new Point(696592, 696592, 8), 3);
-
-		while (iter.hasNext()) {
-			int expect = 0;
-			WritableComparableKey key = iter.next();
-			System.out.println(key);
-			OctreeNode cur = null;
-			// FileOutputStream fos = new FileOutputStream("/tmp/visit.log");
-			// System.setOut(new PrintStream(fos));
-			// 遍历所有的node，找到相关的segs
-			IOctreeIterator scanner = reader.getPostingListScanner(key);
-			while (scanner.hasNext()) {
-				cur = scanner.nextNode();
-				code = cur.getEncoding();
-				if (code.getX() < te && code.getY() + code.getEdgeLen() > ts) {
-					expectNodeNum++;
-					// System.out.print("hit ");
-					System.out.println(code);
-					System.out.println(((DiskOctreeIterator) scanner).nextBucketID);
-				} else {
-					// System.out.print("not hit ");
-				}
-
-				// System.out.println(code);
-
-				boolean print = false;
-
-				for (MidSegment seg : cur.getSegs()) {
-					if (seg.getStart() <= window.getEnd() && seg.getEndTime() >= window.getStart()) {
-						expect++;
-						if (!print) {
-							expectCodes.add(code);
-							if (expectCodes.contains(exCode)) {
-								System.out.println();
-							}
-							print = true;
-						}
-					}
-				}
-				if (expectCodes.contains(exCode)) {
-					System.out.println();
-				}
+		// 遍历所有的posting list
+		try {
+			while (iter.hasNext()) {
+				WritableComparableKey key = iter.next();
+				int[] expect = ExpectedResult(key, reader, window);
+				int[] answer = queryResult(key, reader, window);
+				// fos.close();
+				System.out.println("expected size:" + expect[0] + "," + expect[1]);
+				Assert.assertEquals(expect[0], answer[0]);
+				Assert.assertEquals(expect[1], answer[1]);
 			}
-
-			// fos.close();
-
-			// fos = new FileOutputStream("/tmp/skip.log");
-			// System.setOut(new PrintStream(fos));
-			System.out.println(" skip");
-			int size = 0;
-			scanner = reader.getPostingListIter(key, window.getStart(), window.getEnd());
-			while (scanner.hasNext()) {
-				boolean print = false;
-				cur = scanner.nextNode();
-				code = cur.getEncoding();
-				if (code.getX() < te && code.getY() + code.getEdgeLen() > ts) {
-					expectNodeNum++;
-					// System.out.println(((OctreePostingListIter)
-					// scanner).nextID);
-					System.out.println(code);
-				}
-				for (MidSegment seg : cur.getSegs()) {
-					if (seg.getStart() <= window.getEnd() && seg.getEndTime() >= window.getStart()) {
-						size++;
-						if (!print) {
-							if (!expectCodes.remove(code)) {
-								System.out.println("new visit++++" + code);
-							}
-							print = true;
-						}
-					}
-				}
-			}
-			for (Encoding encoding : expectCodes) {
-				System.out.println("not visisted " + encoding);
-			}
-			// fos.close();
-			System.out.println("expected size:" + expect);
-			Assert.assertEquals(expect, size);
+		} finally {
+			reader.close();
 		}
 	}
 }
