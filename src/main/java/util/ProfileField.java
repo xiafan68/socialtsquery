9d4734fdbf226dcd4d5779f161c514b5cab6ec91
xiafan <xiafan68@gmail.com>
2016-12-27 21:10:47 +0800

    增加代码统计访问的level

--- src/main/java/util/ProfileField.java
+++ src/main/java/util/ProfileField.java
@@ -1,16 +1,15 @@
 package util;
 
 public enum ProfileField {
-	TOTAL_TIME,
-	UPDATE_CAND,//更新当前cand的状态
-	MAINTAIN_CAND,//更新所有cand对象的状态
-	
+	TOTAL_TIME, UPDATE_CAND, // 更新当前cand的状态
+	MAINTAIN_CAND, // 更新所有cand对象的状态
+
 	// io related fields
-	READ_BLOCK,//time 
+	READ_BLOCK, // time
 	NUM_BLOCK,
 
 	// fields for # of records
-	TOPK, CAND, WASTED_REC
-	
-	
+	TOPK, CAND, WASTED_REC,
+
+	LEVELS, HITTED_LEVELS, HITTED_LEVEL_NUM
 }
